9935012d748a69e094152f97e2f17f816d8c3981
rafacamarotti <110203753+rafacamarotti@users.noreply.github.com>
2022-12-21 20:39:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -20,23 +20,33 @@ console.log(Content);
 if(Content == "tire minha selfie"){
     console.log("tirando selfie --- ");
     speak();
-    speakk();
-    speeak();
 }
 }
 function speak(){
-    imgId="selfie";
-    imgid="selfie1";
-    Imgid="selfie2";
-    synth= window.SpeechSynthesis;
+ 
+    synth= window.speechSynthesis;
     var speakData ="Tirando sua selfie em 5 segundos";
     utterThis = new SpeechSynthesisUtterance(speakData);
     synth.speak(utterThis);
     Webcam.attach(camera);
     setTimeout(function(){
+    imgId="selfie";
+      speakData="tirando sua selfie em 5 segundos"
         takeSelfie();
-        save()
+      utterThis = new SpeechSynthesisUtterance(speakData);
+      synth.speak(utterThis);
     },5000);
+    setTimeout(function(){
+    imgId="selfie1";
+      speakData="tirando sua selfie em 5 segundos"
+        takeSelfie();
+      utterThis = new SpeechSynthesisUtterance(speakData);
+      synth.speak(utterThis);
+    },10000);
+    setTimeout(function(){
+    imgId="selfie2"
+        takeSelfie();
+    },15000);
 }
 
 camera = document.getElementById("camera");
@@ -50,12 +60,12 @@ Webcam.set({
 function takeSelfie(){
     Webcam.snap(function(data_uri){
        if(imgId=="selfie"){
-         document.getElementById("result").innerHTML='<img id="selfieImage" src="'+data_uri+'"/>';
+         document.getElementById("result").innerHTML='<img id="selfie" src="'+data_uri+'"/>';
        }
-       if(imgid=="selfie1"){
-         document.getElementById("result").innerHTML='<img id="selfieImage" src="'+data_uri+'"/>';
-    }if(Imgid=="selfie2"){
-         document.getElementById("result").innerHTML='<img id="selfieImage" src="'+data_uri+'"/>';}
+       if(imgId=="selfie1"){
+         document.getElementById("result").innerHTML='<img id="selfie1" src="'+data_uri+'"/>';
+    }if(imgId=="selfie2"){
+         document.getElementById("result").innerHTML='<img id="selfie2" src="'+data_uri+'"/>';}
     });
 
 function save(){
